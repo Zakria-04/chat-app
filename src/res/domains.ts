1bac3da42e0bfc30fa31e966a399263fb2bfdf6f
Zakria-04 <zakriaabdelgny04@gmail.com>
2024-11-24 14:20:31 +0200

    Update domains.ts

--- src/res/domains.ts
+++ src/res/domains.ts
@@ -1,4 +1,3 @@
-export const MainDomain =
-  "https://chat-5f6sh115m-zakria-04s-projects.vercel.app";
+export const MainDomain ="https://chat-au0tozucf-zakria-04s-projects.vercel.app";
 
 // export const MainDomain = "http://localhost:8080";
